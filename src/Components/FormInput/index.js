6eafd03a64c568f39196ec87a8a1d6849ac57542
Robson Rocha <robsom06@hotmail.com>
2021-02-24 06:32:51 -0300

    Todos os arquivos com config

--- src/Components/FormInput/index.js
+++ src/Components/FormInput/index.js
@@ -1,7 +1,17 @@
+import { useEffect } from 'react'
+import $ from 'jquery'
+import 'jquery-mask-plugin/dist/jquery.mask.min'
 import { ErrorMessage } from '@hookform/error-message'
 
 import './styles.css'
 const FormInput = ({register, errors, label, id, name, type, ...inputProps}) => {
+
+  useEffect(() => {
+    $('.money').mask("#.##0,00", { reverse: true, placeholder: '0,00' })
+    $('.emission').mask("00/00/0000", { placeholder: 'Emissão'})
+    $('.due').mask("00/00/0000", { placeholder: 'Vencimento'})
+    $('.empenho').mask("0000NE000000", { placeholder: '____NE______'})
+  })
 
   return (
     <div id="formInput" >
--- src/Pages/Document/index.js
+++ src/Pages/Document/index.js
@@ -1,13 +1,21 @@
 import swal from 'sweetalert'
-import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { useEffect, useState } from 'react'
 import Notch from '../../Components/Notch'
 import { Link, useHistory } from 'react-router-dom'
 import api from '../../Services/api'
 import { swalErrorToken } from '../../Utils/swalAlert'
+import { AiTwotonePlusCircle } from "react-icons/ai";
+import { TiArrowLeftOutline } from 'react-icons/ti'
+
+import FormInput from '../../Components/FormInput'
 
 
 import './styles.css'
 const Document = () => {
+  const { register, handleSubmit, errors, formState } = useForm()
+  const [documents, setDocuments] = useState([])
+  const [formDocument, setFormDocument] = useState(false)
   const history = useHistory()
   const token = localStorage.getItem('token')
   
@@ -22,26 +30,112 @@ const Document = () => {
       })
     })
   }, [token, history])
-  
+
+
+  useEffect(() => {
+    api.get('/document', { headers: { authorization: token } }).then(response => {
+      setDocuments(response.data)
+    })
+  }, [token])
+
+  const { isSubmitting } = formState;
+
+  const onSubmit = async (data) => {
+    console.log(data)
+  }
+
   return (
     <div id="document">
-      <div className="content-header-document">
+      <div className="header-document">
+        <Link to="/home">
+          <TiArrowLeftOutline size={30} color="#2f3136" />
+        </Link>
+        <Notch />
+      </div>
+      <div className="content-table-document">
 
+      <div className="card-new-document" onClick={() => formDocument ? setFormDocument(false) : setFormDocument(true)}>
+        <AiTwotonePlusCircle size={86} color="#2f3136" />
+        <span>Novo Documento</span>
+      </div>
+        {documents.map(document => (
+          <div className="card-document" key={document.id}>
+            <h1>{document.number}</h1>
+            <b>{document.creditor.reason}</b>
+            <b>{document.creditor.code}</b>
+            <p><span>Número</span>{document.number}</p>
+            <p><span>Emissão</span>{document.emission}</p>
+            {document.due && <p><span>Vencimento</span>{document.due}</p> }
+            <p><span>Valor</span>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(document.value)}</p>
+          </div>
+        ))}
+    
       </div>
 
-      <div className="content-body-document">
-        <div className="body">
-          <div className="header-body">
-            <div className="link-header">
-              <Link to="/home">Voltar</Link>
-            </div>
-            <Notch />
-          </div>
-          <div className="content-body">
-           
-          </div>
+      { formDocument && 
+      <div className="new-document">
+        <div className="form-document">
+        <h3>Novo Documento</h3>
+        <span>_Fatura _Nota Fiscal</span>
+          <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+            <FormInput 
+              type="text" 
+              name="number"
+              placeholder="Número Documento"
+              readOnly={isSubmitting ? true : false }
+              register={register({ required: 'Número do documento é abrigatório' })}
+              errors={errors} />
+
+            <FormInput 
+              type="text" 
+              name="emission"
+              readOnly={isSubmitting ? true : false }
+              register={register({ 
+                required: 'Emissão abrigatória',
+                minLength: {
+                  value: 10,
+                  message: 'Emissão requer 10 caracteres'
+                },
+                maxLength: {
+                  value: 10,
+                  message: 'Emissão requer 10 caracteres'
+                }
+              })}
+              errors={errors}
+              className="emission" />
+            
+            <FormInput 
+              type="text" 
+              name="due"
+              readOnly={isSubmitting ? true : false }
+              register={register({ 
+                minLength: {
+                  value: 10,
+                  message: 'Vencimento requer 10 caracteres'
+                },
+                maxLength: {
+                  value: 10,
+                  message: 'Vencimento requer 10 caracteres'
+                }
+              })}
+              errors={errors}
+              className="due" />
+
+            <FormInput 
+              type="text" 
+              name="value"
+              readOnly={isSubmitting ? true : false }
+              register={register({ required: 'Valor é abrigatório' })}
+              errors={errors}
+              className="money" />
+
+
+            <button type="submit">Cadastrar</button>
+          </form>
+          <div className="close" onClick={() => formDocument && setFormDocument(false) } >Fechar</div>
         </div>
       </div>
+      }
     </div>
   )
 }
