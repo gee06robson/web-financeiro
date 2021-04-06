3ea7900abf5b50b2f30f598a5e7e928f983f9822
Robson Rocha <robsom06@hotmail.com>
2021-04-06 20:53:38 -0300

    edição do map unity

--- src/Components/Unity/index.js
+++ src/Components/Unity/index.js
@@ -1,38 +1,21 @@
-import { useEffect, useState } from 'react'
 import { Controller } from  'react-hook-form'
 import Select from 'react-select'
 
 import api from '../../Services/api'
 import './styles.css'
 const Unity = ({ control }) => {
-  const [options, setOptions] = useState([
-  { 
-    value: 'a457f8b2-aac6-472d-ae96-ea1110de1022', 
-    label: 'Almoxarifado' 
-  },
-  {
-    value: '8496e8c2-6cdc-4941-89f4-90e398f196f3', 
-    label: 'Aprovisionamento' 
-  },
-  {
-    value: '1dda3fff-5892-4cce-817f-0a023347d214', 
-    label: 'Setor Financeiro' 
-  }])
-  const [state, setState] = useState(true)
-
-  useEffect(() => {
-    api.get('/allunits').then(response => {
-      setOptions(response.data)
-      setState(false)
-    })
-  }, [])
-
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+  
   return (
   <div id="unity">
     <Controller
       as={Select}
       isClearable
-      isLoading={state}
+      // isLoading={state}
       className='react-select-container'
       classNamePrefix="react-select"
       name="unity"
